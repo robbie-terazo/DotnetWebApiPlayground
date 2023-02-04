98fa2e8f248b31591d21d51f9680d2a7b61ac33d
Robbie Biesser <Robbie.Biesser@terazo.com>
2023-02-04 15:48:07 -0500

    dotnet new webapi

---
+++ WeatherForecast.cs
@@ -0,0 +1,12 @@
+namespace DotnetWebApiPlayground;
+
+public class WeatherForecast
+{
+    public DateTime Date { get; set; }
+
+    public int TemperatureC { get; set; }
+
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+    public string? Summary { get; set; }
+}
